79a0d1de253f28e20f074bc32520da771ca5d9ef
EliIntern <intern.elogicinnovations@gmail.com>
2025-06-18 00:30:09 +0800

    Initial project structure with frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', // default for Laragon
+  database: 'sample_crud'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Connected to MySQL');
+});
+
+// Read
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) res.status(500).send(err);
+    else res.json(results);
+  });
+});
+
+// Create
+app.post('/users', (req, res) => {
+  const { name, email } = req.body;
+  db.query('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], (err, result) => {
+    if (err) res.status(500).send(err);
+    else res.send('User added');
+  });
+});
+
+// Delete
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.query('DELETE FROM users WHERE id = ?', [id], (err, result) => {
+    if (err) res.status(500).send(err);
+    else res.send('User deleted');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SampleUpload from './Pages/SampleUpload';
+
+function App() {
+  return (
+
+    <Router>
+      
+        <Routes>
+          <Route path="/" element={<SampleUpload/>} />
+        
+        </Routes>
+      
+    </Router>
+
+    
+  );
+}
+
+export default App;
